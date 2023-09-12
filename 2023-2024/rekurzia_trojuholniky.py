74ddc7f77e1a20781aafe58909167df1b072d554
Castrolicek <hawardjoj05@gmail.com>
2023-09-12 09:35:29 +0200

    news

---
+++ 2023-2024/rekurzia_trojuholniky.py
@@ -0,0 +1,11 @@
+# uloha urobit trojuholnik v trojuholnikoch
+import tkinter
+import random
+c = tkinter.Canvas(height = 1000, width = 1000)
+def polygon():
+
+
+c.create_polygon(0,1000,1000,1000,500,0, fill = 'yellow')
+polygon(0,)
+c.pack()
+c.mainloop()
