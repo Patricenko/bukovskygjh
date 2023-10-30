0234705a0290af1eecf12a0697adf2fa6ff3c8a0
Patricenko <bukovsky.p@gjh.sk>
2023-10-30 17:52:42 +0100

    megalevels

--- 2023-2024/pexeso/hra.py
+++ 2023-2024/pexeso/hra.py
@@ -1,13 +1,37 @@
-import tkinter
+from tkinter import Canvas, Tk, Menu, Button
 import modul
+
+def load1():
+    current = modul.Pexeso(o, a, c, 1)
+def load2():
+    current = modul.Pexeso(o, a, c, 2)
+def load3():
+    current = modul.Pexeso(o, a, c, 3)
+def load4():
+    current = modul.Pexeso(o, a, c, 4)
+def load5():
+    a = 150
+    current = modul.Pexeso(o, a, c, 5)
+
 
 o = 10
 a = 200
 
-c = tkinter.Canvas(width = 2*o+a*5, height = 2*o+a*4)
+main = Tk()
+main.title('PEXESO')
+level = Menu(main)
+level.add_command(label = f'Level 1', command = load1)
+level.add_command(label = f'Level 2', command = load2)
+level.add_command(label = f'Level 3', command = load3)
+level.add_command(label = f'Level 4', command = load4)
+level.add_command(label = f'Level 5', command = load5)
+main.config(menu = level)
+
+c = Canvas(main)
+c.create_text(200,50,text='Pexeso by Patrik Bukovský 3.C')
+c.create_text(200,70,text='Ak vyhráš, automaticky postupuješ na ďalší level.')
+c.create_text(200,90,text='Vieš aj prechádzať medzi levelmi hore na paneli.')
+klik = Button(c,text='Začať!', command = lambda:[load1(),klik.destroy()]); klik.place(x=175,y=110)
 c.pack()
-current = modul.Pexeso(o, a, c)
-c.bind('<Button-1>', current.click)
 
-
-c.mainloop()
+main.mainloop()
--- 2023-2024/pexeso/modul.py
+++ 2023-2024/pexeso/modul.py
@@ -1,12 +1,29 @@
 import random, time
 class Pexeso:
-    def __init__(self, o, a, c):
+    def __init__(self, o, a, c, level):
         self.plocha = []
         self.c = c
         self.a = a
         self.o = o
         self.colors = ['red','yellow','blue','lime','black']
-        self.items = ['Arthas','Jaina','Thrall','Sylvannas','Archimonde', 'Kelthuzad','Algalon','Raszageth','Terenas','Abberus']
+        self.choice = ['Arthas','Jaina','Thrall','Sylvannas','Archimonde', 'Kelthuzad','Algalon','Raszageth','Terenas','Abberus','Halion','Lich King','Sarkareth','Illidan','Darion']
+        self.items = []
+        self.level = level
+        match level:
+            case 1:
+                self.a0,self.b0=4,4
+            case 2:
+                self.a0,self.b0=4,5
+            case 3:
+                self.a0,self.b0=4,6
+            case 4:
+                self.a0,self.b0=4,7
+            case 5:
+                self.a0, self.b0 = 5, 6
+        for i in range(int((self.a0 * self.b0) / 2)): self.items.append(self.choice[i])
+        self.c.delete('all')
+        self.c.config(width=2 * o + a * self.b0, height=2 * o + a * self.a0)
+        self.c.bind('<Button-1>', self.click)
         self.list = 2*self.items
         self.active = None
         self.done = []
@@ -24,9 +41,9 @@ class Pexeso:
         a = self.a
         o = self.o
         x = 0
-        for i in range(4):
+        for i in range(self.a0):
             row = []
-            for j in range(5):
+            for j in range(self.b0):
                 x += 1
                 s = (self.Karta(j*a, i*a, x))
                 s.shape = self.c.create_rectangle(o + j * a, o + i * a, o + j * a + a, o + i * a + a, fill = 'red')
@@ -35,33 +52,33 @@ class Pexeso:
             self.plocha.append(row)
 
     def randomize(self):
-        print(len(self.plocha),len(self.plocha[0]))
         random.shuffle(self.list)
         x = 0
-        for i in range (4):
-            for j in range (5):
+        for i in range (self.a0):
+            for j in range (self.b0):
                 s = self.plocha[i][j]
                 self.c.itemconfig(s.obsah, text=self.list[x], fill=self.colors[0])
                 s.value = self.list[x]
                 x += 1
     def YouWon(self):
-        for i in range (4):
-            for j in range (5):
+        for i in range (self.a0):
+            for j in range (self.b0):
                 s = self.plocha[i][j]
                 self.c.itemconfig(s.shape, fill=self.colors[2])
-        wintime = self.c.create_text(self.o + self.a * 2.5, self.o + self.a * 2, font=('Arial bold', 200), fill=self.colors[0], text='5')
+        wintime = self.c.create_text(self.o + self.a * self.b0/2, self.o + self.a * self.a0/2, font=('Arial bold', 200), fill=self.colors[0], text='5')
         self.c.update()
         time.sleep(1)
         for i in range (-4,0,1):
             self.c.itemconfig(wintime, text=-i)
             self.c.update()
             time.sleep(1)
-
-        self.__init__(self.o,self.a,self.c)
+        if self.level == 5:
+            return
+        self.__init__(self.o,self.a,self.c,self.level+1)
     def click(self, sur):
         a = self.a
-        for i in range (4):
-            for j in range (5):
+        for i in range (self.a0):
+            for j in range (self.b0):
                 s = self.plocha[i][j]
                 if sur.x >= s.x and sur.x <= s.x+a:
                     if sur.y >= s.y and sur.y <= s.y+a:
@@ -80,7 +97,7 @@ class Pexeso:
                                 self.done.append(s)
 
                                 self.active = None
-                                if len(self.done) == 20:
+                                if len(self.done) == self.a0*self.b0:
                                     self.YouWon()
                             elif self.active.value == s.value and self.active == s:
                                 return
