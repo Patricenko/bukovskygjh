9298e7a0090be90e7310710d1b59b27147e38627
Olejnicka <bukovsky.p@gjh.sk>
2024-04-08 23:29:59 +0200

    getget

--- 2023-2024/Rodokmeň/core.py
+++ 2023-2024/Rodokmeň/core.py
@@ -4,25 +4,30 @@ from modul import *
 def createbuttons(generations, current, lastpos, string):
     if current > generations: return
     posx = WIDTH//(2**current)
-    TButton(win, text="Nič", x=lastpos+posx, y=50*current-50, level=current, bt=strom, pos=string+'1')
-    TButton(win, text="Nič", x=lastpos-posx, y=50*current-50, level=current, bt=strom, pos=string+'2')
+    c.create_line(lastpos+20,50*(current-1)-35, lastpos+posx+20,50*current-35,fill="red",width=5)
+    c.create_line(lastpos+20,50*(current-1)-35, lastpos-posx+20,50*current-35,fill="blue",width=5)
+    TButton(c, text="Nič", x=lastpos+posx, y=50*current-50, level=current, bt=strom, pos=string+'1')
+    TButton(c, text="Nič", x=lastpos-posx, y=50*current-50, level=current, bt=strom, pos=string+'2')
     createbuttons(generations, current + 1, lastpos + posx, string + '1')
     createbuttons(generations, current + 1, lastpos - posx, string + '2')
-def draw_tree(g):
+def createTree(g):
+    global strom, c
     g = int(g)
-    global HEIGHT, WIDTH, strom
-    HEIGHT = 600
-    WIDTH = 1500
-    win.geometry(f"{WIDTH}x{HEIGHT}")
     win.deiconify()
+    c = tkinter.Canvas(win, width=WIDTH, height=HEIGHT)
+    c.pack()
     strom = BinaryTree(levels=g)
     TButton(win, text="Nič",x=(WIDTH-50)//2, y=0, bt=strom)
     createbuttons(g,2, (WIDTH-50)//2, "")
 
+
+HEIGHT = 600
+WIDTH = 1500
 win = tkinter.Tk()
+win.geometry(f"{WIDTH}x{HEIGHT}")
 win.title("Rodokmen")
 print("Pre tento WIDTH max 6 levelov")
 print("Po UPDATE  tlačítka sa ukážu v termináli dve zobrazenia Binárneho Stromu")
 win.withdraw()
-create_input_window("Number of generations:", callback=draw_tree)
+create_input_window("Number of generations:", callback=createTree)
 win.mainloop()
