1bca2668aa20535bd6d8dde707fd81fae59da16e
Olejnicka <bukovsky.p@gjh.sk>
2024-04-08 21:48:03 +0200

    geget

--- 2023-2024/Rodokmeň/modul.py
+++ 2023-2024/Rodokmeň/modul.py
@@ -1,5 +1,5 @@
 import tkinter
-class Node:
+class Vertex:
     def __init__(self, value=None):
         self.value = value
         self.left = None
@@ -10,72 +10,72 @@ class BinaryTree:
         self.root = None
         self.levels = levels
         if levels > 0:
-            self.root = self._create_complete_tree(levels)
-    def _create_complete_tree(self, levels, current_level=0):
+            self.root = self.createTree(levels)
+    def createTree(self, levels, current_level=0):
         if current_level >= levels:
             return None
-        root = Node()
-        root.left = self._create_complete_tree(levels, current_level + 1)
-        root.right = self._create_complete_tree(levels, current_level + 1)
+        root = Vertex()
+        root.left = self.createTree(levels, current_level + 1)
+        root.right = self.createTree(levels, current_level + 1)
         return root
 
     def update_value(self, path, value):
         moves = [int(digit) for digit in str(path)]
-        return self._update_value_by_path_recursive(self.root, moves, value)
+        return self.rekurzia(self.root, moves, value)
 
-    def _update_value_by_path_recursive(self, node, moves, value):
-        if node is None or not moves:
-            node.value = value
+    def rekurzia(self, vertex, moves, value):
+        if vertex is None or not moves:
+            vertex.value = value
             return True
         if len(moves) == 1:
             if moves[0] == 2:
-                if node.left is not None:
-                    node.left.value = value
+                if vertex.left is not None:
+                    vertex.left.value = value
                     return True
                 else:
-                    node.left = Node(value)
+                    vertex.left = Vertex(value)
                     return True
             elif moves[0] == 1:
-                if node.right is not None:
-                    node.right.value = value
+                if vertex.right is not None:
+                    vertex.right.value = value
                     return True
                 else:
-                    node.right = Node(value)
+                    vertex.right = Vertex(value)
                     return True
             return False
 
         if moves[0] == 2:
-            return self._update_value_by_path_recursive(node.left, moves[1:], value)
+            return self.rekurzia(vertex.left, moves[1:], value)
         elif moves[0] == 1:
-            return self._update_value_by_path_recursive(node.right, moves[1:], value)
+            return self.rekurzia(vertex.right, moves[1:], value)
         else:
             return False
-    def print_tree(self, node=None, level=0):
+    def print_tree(self, vertex=None, level=0):
         if level == 0: print("##########################################################################")
         if level > self.levels: return
-        if node is None:
-            node = self.root
-        if node is not None:
-            self.print_tree(node.right, level + 1)
-            print(' ' * 4 * level + '->', node.value)
-            self.print_tree(node.left, level + 1)
+        if vertex is None:
+            vertex = self.root
+        if vertex is not None:
+            self.print_tree(vertex.right, level + 1)
+            print(' ' * 4 * level + '->', vertex.value)
+            self.print_tree(vertex.left, level + 1)
 
     def print_tree2(self):
         print("##########################################################################")
         levels = []
-        self._collect_levels(self.root, 0, levels)
+        self.rekurzia2(self.root, 0, levels)
         for level in levels:
             print(" ".join(level))
         print("##########################################################################")
 
-    def _collect_levels(self, node, depth, levels):
-        if node is None:
+    def rekurzia2(self, vertex, depth, levels):
+        if vertex is None:
             return
         if len(levels) <= depth:
             levels.append([])
-        levels[depth].append(str(node.value))
-        self._collect_levels(node.left, depth + 1, levels)
-        self._collect_levels(node.right, depth + 1, levels)
+        levels[depth].append(str(vertex.value))
+        self.rekurzia2(vertex.left, depth + 1, levels)
+        self.rekurzia2(vertex.right, depth + 1, levels)
 
 class TButton:
     def __init__(self, win, text, x, y, level=0, bt=None, pos=""):
